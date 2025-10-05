e39c0353ed92c80c0b8175358329d4c76687ae72
Eliel-DM <elieldiasmatos@gmail.com>
2025-10-05 10:44:27 -0300

    Criado as migrations responsáveis por criar as tablelas relacionadas aos produtos no banco de dados.

--- src/migration/1759668379060-CreateProductsTable.ts
+++ src/migration/1759668379060-CreateProductsTable.ts
@@ -1,11 +1,67 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from "typeorm";
 
-export class CreateProductsTable1759668379060 implements MigrationInterface {
+export class CreateProductsTable1759668420000 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "products",
+        columns: [
+          {
+            name: "id",
+            type: "int",
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: "increment",
+          },
+          { name: "name", type: "varchar", isUnique: true },
+          { name: "productCategoryId", type: "int" },
+          { name: "productSituationId", type: "int" },
+          {
+            name: "createdAt",
+            type: "timestamp",
+            default: "CURRENT_TIMESTAMP",
+          },
+          {
+            name: "updatedAt",
+            type: "timestamp",
+            default: "CURRENT_TIMESTAMP",
+            onUpdate: "CURRENT_TIMESTAMP",
+          },
+        ],
+      })
+    );
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey(
+      "products",
+      new TableForeignKey({
+        columnNames: ["productCategoryId"],
+        referencedColumnNames: ["id"],
+        referencedTableName: "product_categories",
+        onDelete: "CASCADE",
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      "products",
+      new TableForeignKey({
+        columnNames: ["productSituationId"],
+        referencedColumnNames: ["id"],
+        referencedTableName: "product_situations",
+        onDelete: "CASCADE",
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const table = await queryRunner.getTable("products");
+    if (table) {
+      await queryRunner.dropForeignKeys("products", table.foreignKeys);
     }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+    await queryRunner.dropTable("products");
+  }
 }
---
+++ src/migration/1759668410000-CreateProductsCategoriesTable.ts.ts
@@ -0,0 +1,43 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateProductsCategoriesTable1759668410000
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "product_categories",
+        columns: [
+          {
+            name: "id",
+            type: "int",
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: "increment",
+          },
+          {
+            name: "name",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "createdAt",
+            type: "timestamp",
+            default: "CURRENT_TIMESTAMP",
+          },
+          {
+            name: "updatedAt",
+            type: "timestamp",
+            default: "CURRENT_TIMESTAMP",
+            onUpdate: "CURRENT_TIMESTAMP",
+          },
+        ],
+      }),
+      true // <- este parâmetro `true` evita erros se a tabela já existir
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("product_categories");
+  }
+}
